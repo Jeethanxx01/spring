a4929b938280c73b258d2cb6c2334964e5bdf2a8
Jeethan crasta <jeethancs@gmail.com>
2024-11-18 11:26:01 +0530

    calculator

---
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -0,0 +1,43 @@
+package com.example.calculator.controller;
+
+import com.example.calculator.service.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @GetMapping("/add")
+    public double add(@RequestParam double a,@RequestParam double b){
+        return calculatorService.add(a,b);
+    }
+
+    @GetMapping("/subtract")
+    public double subtract(@RequestParam double a,@RequestParam double b){
+        return calculatorService.subtract(a,b);
+    }
+
+    @GetMapping("/multiply")
+    public double multiply(@RequestParam double a,@RequestParam double b){
+        return calculatorService.multiply(a,b);
+    }
+
+    @GetMapping("/divide")
+    public double divide(@RequestParam double a,@RequestParam double b){
+        return calculatorService.divide(a,b);
+    }
+
+    @GetMapping("/modulus")
+    public double modulus(@RequestParam double a,@RequestParam double b){
+        return calculatorService.modulus(a,b);
+    }
+
+
+}
